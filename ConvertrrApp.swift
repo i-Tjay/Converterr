83a2d6616595510319ea5283712a98e1230d6443
TJ <100756836+i-Tjay@users.noreply.github.com>
2022-04-18 05:34:44 -0700

    Add files via upload

---
+++ ConvertrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertrrApp.swift
+//  Convertrr
+//
+//  Created by Mac on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConvertrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
